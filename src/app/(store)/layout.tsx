a71e77ebeda11f32065d28974c1f4212bf4c1ff7
Andre <andreappereira@hotmail.com>
2024-04-13 23:17:10 -0300

    Created header component.

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react';
+import { Header } from 'src/components/header';
+
+type StoreLayoutProps = {
+  children: ReactNode;
+};
+
+const StoreLayout: React.FC<StoreLayoutProps> = ({ children }) => (
+  <div className="mx-auto grid min-h-screen w-full max-w[1600px] grid-rows-app gap-5 px-8 p-8">
+    <Header />
+    {children}
+  </div>
+);
+
+export default StoreLayout;
